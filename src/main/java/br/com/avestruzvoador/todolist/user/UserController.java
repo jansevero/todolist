7ccfc97a71ea0b15a41314f6197442a54df212e5
jansevero <46618122+jansevero@users.noreply.github.com>
2023-10-14 19:25:25 -0300

    Initial commit

---
+++ src/main/java/br/com/avestruzvoador/todolist/user/UserController.java
@@ -0,0 +1,32 @@
+package br.com.avestruzvoador.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  
+  @Autowired
+  private IUserRepository userRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      //mensagem de erro
+      //status code
+      return ResponseEntity.status(400).body("Usuário já existe");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
+}
---
+++ src/test/java/br/com/avestruzvoador/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avestruzvoador.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
