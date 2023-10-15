3f90063ec499878f917eacf62cf7aa7ed251766c
jansevero <46618122+jansevero@users.noreply.github.com>
2023-10-15 01:21:55 -0300

    aula 04

--- src/main/java/br/com/avestruzvoador/todolist/user/UserController.java
+++ src/main/java/br/com/avestruzvoador/todolist/user/UserController.java
@@ -1,13 +1,14 @@
 package br.com.avestruzvoador.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -23,8 +24,12 @@ public class UserController {
     if(user != null) {
       //mensagem de erro
       //status code
-      return ResponseEntity.status(400).body("Usuário já existe");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray())  ;
+
+    userModel.setPassword(passwordHashed);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
